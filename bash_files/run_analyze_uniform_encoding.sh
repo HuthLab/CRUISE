e5ac59ca9c4450e7df319eee84961de02f94a525
mujn1461 <mjnlinda0527@gmail.com>
2025-09-10 14:56:01 -0700

    change paths

--- bash_files/run_analyze_uniform_encoding.sh
+++ bash_files/run_analyze_uniform_encoding.sh
@@ -21,9 +21,9 @@ for story in "${stories[@]}"
 do
     echo "Processing $story"
     if [ "$random_recalls" == "true" ]; then
-        python ../analyze_uniform_encoding.py --model "$model" --adjusted --random_recalls --story "$story" --save_dir ../../generated --segmentation_dir ../../data/behavior_data/segmentation --original_transcript_dir ../../data/behavior_data/transcripts/moth_stories --exclusion_dir ../../data/behavior_data/exclusion --timing_dir ../../data/behavior_data/transcripts/timing
+        python ../analyze_uniform_encoding.py --model "$model" --adjusted --random_recalls --story "$story" --save_dir ../../generated --segmentation_dir ../../behavior_data/segmentation --original_transcript_dir ../../behavior_data/transcripts/moth_stories --exclusion_dir ../../behavior_data/exclusion --timing_dir ../../behavior_data/transcripts/timing
     else
-        python ../analyze_uniform_encoding.py --model "$model" --adjusted --story "$story" --save_dir ../../generated --segmentation_dir ../../data/behavior_data/segmentation --original_transcript_dir ../../data/behavior_data/transcripts --exclusion_dir ../../data/behavior_data/exclusion --timing_dir ../../data/behavior_data/transcripts/timing
+        python ../analyze_uniform_encoding.py --model "$model" --adjusted --story "$story" --save_dir ../../generated --segmentation_dir ../../behavior_data/segmentation --original_transcript_dir ../../behavior_data/transcripts --exclusion_dir ../../behavior_data/exclusion --timing_dir ../../behavior_data/transcripts/timing
     fi
 done
 
